0fc1897373fd66153b8ea53541bdf23367c2da41
George Chondrompilas <gchondrompilas@vwd.com>
2020-01-21 05:03:18 +0200

    WASD movement implemented

--- Assets/MoveController.cs
+++ Assets/MoveController.cs
@@ -19,19 +19,19 @@ public class MoveController : MonoBehaviour
     // Update is called once per frame
  void Update()
  {
-     if(Input.GetKey(KeyCode.RightArrow))
+     if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
      {
          transform.Translate(new Vector3(speed * Time.deltaTime,0,0));
      }
-     if(Input.GetKey(KeyCode.LeftArrow))
+     if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
      {
          transform.Translate(new Vector3(-speed * Time.deltaTime,0,0));
      }
-     if(Input.GetKey(KeyCode.DownArrow))
+     if(Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
      {
          transform.Translate(new Vector3(0,0,-speed * Time.deltaTime));
      }
-     if(Input.GetKey(KeyCode.UpArrow))
+     if(Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
      {
          transform.Translate(new Vector3(0,0,speed * Time.deltaTime));
      }
