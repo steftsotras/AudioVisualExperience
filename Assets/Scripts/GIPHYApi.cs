0d0ccba0acdaf2daa63f031077e30044721bbec1
George Chondrompilas <gchondrompilas@vwd.com>
2020-04-19 13:14:04 +0200

    GIPHY API implemented

--- Assets/Scripts/GIPHYApi.cs
+++ Assets/Scripts/GIPHYApi.cs
@@ -1,11 +1,135 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
 
 namespace Assets.Scripts
 {
-    class GIPHYApi
+    public class GIPHYApi
     {
+        private const string RANDOM_ENDPOINT = "https://api.giphy.com/v1/gifs/random";
+        private const string SEARCH_ENDPOINT = "http://api.giphy.com/v1/gifs/search";
+        private const string TRANSLATE_ENDPOINT = "http://api.giphy.com/v1/gifs/translate";
+        private const string TRENDING_ENDPOINT = "http://api.giphy.com/v1/gifs/trending";
+
+        private const string DEFAULT_STRING = "";
+        private const string DEFAULT_RATING = "";
+        private const int DEFAULT_OFFSET = 0;
+        private const int DEFAULT_WEIRDNESS = 0;
+        private const int DEFAULT_LIMIT = 25;
+
+        private string api_key = "";
+
+        public GIPHYApi(string api_key)
+        {
+            this.api_key = api_key;
+        }
+
+        public IEnumerator Random(string tag = DEFAULT_STRING, string rating = DEFAULT_STRING, Action<GIPHYSingleObject> onSuccess = null, Action<string> onError = null)
+        {
+            var qCollection = ToQueryCollection(
+                new QueryParam<string> { name = "api_key", value = api_key },
+                new QueryParam<string> { name = "tag", value = tag, defaultValue = DEFAULT_STRING },
+                new QueryParam<string> { name = "rating", value = rating, defaultValue = DEFAULT_STRING }
+            );
+            return _request(RANDOM_ENDPOINT, qCollection, onSuccess, onError);
+        }
+
+        public IEnumerator Search(string query, int limit = DEFAULT_LIMIT, int offset = DEFAULT_OFFSET, string rating = DEFAULT_STRING, string lang = DEFAULT_STRING, Action<GIPHYCollectionObject> onSuccess = null, Action<string> onError = null)
+        {
+            var qCollection = ToQueryCollection(
+               new QueryParam<string> { name = "api_key", value = api_key },
+               new QueryParam<string> { name = "limit", value = limit.ToString(), defaultValue = DEFAULT_LIMIT.ToString() },
+               new QueryParam<string> { name = "offset", value = offset.ToString(), defaultValue = DEFAULT_OFFSET.ToString() },
+               new QueryParam<string> { name = "rating", value = rating, defaultValue = DEFAULT_STRING }
+            );
+            return _request(SEARCH_ENDPOINT, qCollection, onSuccess, onError);
+        }
+
+        public IEnumerator Trending(int limit = DEFAULT_LIMIT, int offset = DEFAULT_OFFSET, string rating = DEFAULT_RATING, string lang = DEFAULT_STRING, Action<GIPHYCollectionObject> onSuccess = null, Action<string> onError = null)
+        {
+            var qCollection = ToQueryCollection(
+                new QueryParam<string> { name = "api_key", value = api_key },
+                new QueryParam<string> { name = "limit", value = limit.ToString(), defaultValue = DEFAULT_LIMIT.ToString() },
+                new QueryParam<string> { name = "offset", value = offset.ToString(), defaultValue = DEFAULT_OFFSET.ToString() },
+                new QueryParam<string> { name = "rating", value = rating, defaultValue = DEFAULT_STRING },
+                new QueryParam<string> { name = "lang", value = lang, defaultValue = DEFAULT_STRING }
+            );
+            return _request(TRENDING_ENDPOINT, qCollection, onSuccess, onError);
+        }
+
+        public IEnumerator Translate(string s, int weirdness = DEFAULT_WEIRDNESS, Action<GIPHYSingleObject> onSuccess = null, Action<string> onError = null)
+        {
+            var qCollection = ToQueryCollection(
+                new QueryParam<string> { name = "api_key", value = api_key },
+                new QueryParam<string> { name = "s", value = s },
+                new QueryParam<string> { name = "weirdness", value = weirdness.ToString(), defaultValue = DEFAULT_WEIRDNESS.ToString() }
+            );
+            return _request(TRANSLATE_ENDPOINT, qCollection, onSuccess, onError);
+        }
+
+        private IEnumerator _request<T>(string url, Action<T> onSuccess, Action<string> onError)
+        {
+            return _request(url, null, onSuccess, onError);
+        }
+
+        private IEnumerator _request<T>(string url, NameValueCollection queryParams, Action<T> onSuccess, Action<string> onError)
+        {
+
+            if (queryParams != null && queryParams.Count > 0)
+            {
+                url += ToQueryString(queryParams);
+            }
+
+            UnityWebRequest request = UnityWebRequest.Get(url);
+            yield return request.SendWebRequest();
+
+            if (request.isNetworkError || request.isHttpError)
+            {
+                Debug.LogError(request.error);
+                onError(request.error);
+            }
+
+            var result = JsonUtility.FromJson<T>(request.downloadHandler.text);
+            onSuccess(result);
+        }
+
+        private NameValueCollection ToQueryCollection(params QueryParam<string>[] queryParams)
+        {
+            var nvc = new NameValueCollection();
+            foreach (var q in queryParams)
+            {
+                if (q.defaultValue != q.value)
+                {
+                    nvc.Add(q.name, q.value);
+                }
+            }
+            return nvc;
+        }
+
+        private string ToQueryString(NameValueCollection nvc)
+        {
+            var array = (
+                from key in nvc.AllKeys
+                from value in nvc.GetValues(key)
+                select string.Format(
+            "{0}={1}",
+            UnityWebRequest.EscapeURL(key),
+            UnityWebRequest.EscapeURL(value))
+                ).ToArray();
+            return "?" + string.Join("&", array);
+        }
+
+    }
+
+    public class QueryParam<T>
+    {
+        public string name { get; set; }
+        public T value { get; set; }
+        public T defaultValue { get; set; }
     }
 }
---
+++ Assets/Scripts/GIPHYModels.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts
+{
+    public class Still
+    {
+        public string height { get; set; }
+        public string size { get; set; }
+        public string url { get; set; }
+        public string width { get; set; }
+    }
+
+    public class Original
+    {
+        public string frames { get; set; }
+        public string hash { get; set; }
+        public string height { get; set; }
+        public string mp4 { get; set; }
+        public string mp4_size { get; set; }
+        public string size { get; set; }
+        public string url { get; set; }
+        public string webp { get; set; }
+        public string webp_size { get; set; }
+        public string width { get; set; }
+    }
+
+    public class Mp4
+    {
+        public string height { get; set; }
+        public string mp4 { get; set; }
+        public string mp4_size { get; set; }
+        public string width { get; set; }
+    }
+
+    public class Downsampled
+    {
+        public string height { get; set; }
+        public string size { get; set; }
+        public string url { get; set; }
+        public string webp { get; set; }
+        public string webp_size { get; set; }
+        public string width { get; set; }
+    }
+
+    public class Looping
+    {
+        public string mp4 { get; set; }
+        public string mp4_size { get; set; }
+    }
+
+    public class Fixed
+    {
+        public string height { get; set; }
+        public string mp4 { get; set; }
+        public string mp4_size { get; set; }
+        public string size { get; set; }
+        public string url { get; set; }
+        public string webp { get; set; }
+        public string webp_size { get; set; }
+        public string width { get; set; }
+    }
+
+    public class __invalid_type__480wStill
+    {
+        public string url { get; set; }
+        public string width { get; set; }
+        public string height { get; set; }
+    }
+
+    public class Images
+    {
+        public Mp4 hd { get; set; }
+        public Still downsized_large { get; set; }
+        public Still fixed_height_small_still { get; set; }
+        public Original original { get; set; }
+        public Downsampled fixed_height_downsampled { get; set; }
+        public Still downsized_still { get; set; }
+        public Still fixed_height_still { get; set; }
+        public Still downsized_medium { get; set; }
+        public Still downsized { get; set; }
+        public Still preview_webp { get; set; }
+        public Mp4 original_mp4 { get; set; }
+        public Fixed fixed_height_small { get; set; }
+        public Fixed fixed_height { get; set; }
+        public Mp4 downsized_small { get; set; }
+        public Mp4 preview { get; set; }
+        public Downsampled fixed_width_downsampled { get; set; }
+        public Still fixed_width_small_still { get; set; }
+        public Fixed fixed_width_small { get; set; }
+        public Still original_still { get; set; }
+        public Still fixed_width_still { get; set; }
+        public Looping looping { get; set; }
+        public Fixed fixed_width { get; set; }
+        public Still preview_gif { get; set; }
+        public __invalid_type__480wStill __invalid_name__480w_still { get; set; }
+    }
+
+    public class User
+    {
+        public string avatar_url { get; set; }
+        public string banner_image { get; set; }
+        public string banner_url { get; set; }
+        public string profile_url { get; set; }
+        public string username { get; set; }
+        public string display_name { get; set; }
+        public bool is_verified { get; set; }
+    }
+
+    public class Onload
+    {
+        public string url { get; set; }
+    }
+
+    public class Onclick
+    {
+        public string url { get; set; }
+    }
+
+    public class Onsent
+    {
+        public string url { get; set; }
+    }
+
+    public class Analytics
+    {
+        public Onload onload { get; set; }
+        public Onclick onclick { get; set; }
+        public Onsent onsent { get; set; }
+    }
+
+    public class RootImage
+    {
+        public string type { get; set; }
+        public string id { get; set; }
+        public string url { get; set; }
+        public string slug { get; set; }
+        public string bitly_gif_url { get; set; }
+        public string bitly_url { get; set; }
+        public string embed_url { get; set; }
+        public string username { get; set; }
+        public string source { get; set; }
+        public string title { get; set; }
+        public string rating { get; set; }
+        public string content_url { get; set; }
+        public string source_tld { get; set; }
+        public string source_post_url { get; set; }
+        public int is_sticker { get; set; }
+        public string import_datetime { get; set; }
+        public string trending_datetime { get; set; }
+        public Images images { get; set; }
+        public User user { get; set; }
+        public string analytics_response_payload { get; set; }
+        public Analytics analytics { get; set; }
+    }
+
+    public class Pagination
+    {
+        public int total_count { get; set; }
+        public int count { get; set; }
+        public int offset { get; set; }
+    }
+
+    public class Meta
+    {
+        public int status { get; set; }
+        public string msg { get; set; }
+        public string response_id { get; set; }
+    }
+
+    public class GIPHYCollectionObject
+    {
+        public List<RootImage> data { get; set; }
+        public Pagination pagination { get; set; }
+        public Meta meta { get; set; }
+    }
+
+    public class GIPHYSingleObject
+    {
+        public RootImage data { get; set; }
+        public Meta meta { get; set; }
+    }
+}
